86dc952cdebdeebe8fb21acba85636bf240b0282
Ильдар <ildarnigmetzjanov@gmail.com>
2022-10-24 18:46:06 +0300

    Fix problem

--- homework.py
+++ homework.py
@@ -13,15 +13,11 @@ class InfoMessage:
         self.calories = calories
 
     def get_message(self) -> str:
-        res = (f'Тип тренировки: {self.training_type}; ',
-               'Длительность: {:.3f} ч.; '.format(self.duration),
-               'Дистанция: {:.3f} км; '.format(self.distance),
-               'Ср. скорость: {:.3f} км/ч; '.format(self.speed),
-               'Потрачено ккал: {:.3f}.'.format(self.calories))
-        res_text = ""
-        for i in res:
-            res_text += i
-        return res_text
+        return (f'Тип тренировки: {self.training_type}; '
+                f'Длительность: {self.duration:.3f} ч.; '
+                f'Дистанция: {self.distance:.3f} км; '
+                f'Ср. скорость: {self.speed:.3f} км/ч; '
+                f'Потрачено ккал: {self.calories:.3f}.')
 
 
 class Training:
@@ -54,18 +50,17 @@ class Training:
 
     def show_training_info(self) -> InfoMessage:
         """Вернуть информационное сообщение о выполненной тренировке."""
-        info_message = InfoMessage(self.__class__.__name__,
-                                   self.duration,
-                                   self.get_distance(),
-                                   self.get_mean_speed(),
-                                   self.get_spent_calories())
-        return info_message
+        return InfoMessage(self.__class__.__name__,
+                           self.duration,
+                           self.get_distance(),
+                           self.get_mean_speed(),
+                           self.get_spent_calories())
 
 
 class Running(Training):
     """Тренировка: бег."""
-    CALORIES_MEAN_SPEED_MULTIPLIER = 18
-    CALORIES_MEAN_SPEED_SHIFT = 1.79
+    CALORIES_MEAN_SPEED_MULTIPLIER: int = 18
+    CALORIES_MEAN_SPEED_SHIFT: float = 1.79
 
     def __init__(self,
                  action: int,
@@ -84,10 +79,10 @@ class Running(Training):
 
 class SportsWalking(Training):
     """Тренировка: спортивная ходьба."""
-    CALORIES_WEIGHT_MULTIPLIER = 0.035
-    CALORIES_SPEED_HEIGHT_MULTIPLIER = 0.029
-    KMH_IN_MSEC = 0.278
-    CM_IN_M = 100
+    CALORIES_WEIGHT_MULTIPLIER: float = 0.035
+    CALORIES_SPEED_HEIGHT_MULTIPLIER: float = 0.029
+    KMH_IN_MSEC: float = 0.278
+    CM_IN_M: int = 100
 
     def __init__(self,
                  action: int,
@@ -140,19 +135,14 @@ class Swimming(Training):
 
 def read_package(workout_type: str, data: list) -> Training:
     """Прочитать данные полученные от датчиков."""
-    if workout_type == 'SWM':
-        return Swimming(data[0],
-                        data[1],
-                        data[2],
-                        data[3],
-                        data[4])
-    elif workout_type == 'RUN':
-        return Running(data[0], data[1], data[2])
-    else:
-        return SportsWalking(data[0],
-                             data[1],
-                             data[2],
-                             data[3])
+    classes_dict = {'RUN': Running,
+                    'SWM': Swimming,
+                    'WLK': SportsWalking}
+    try:
+        result_class = classes_dict[workout_type]
+        return result_class(*data)
+    except KeyError:
+        raise KeyError('Нет такого типа тренировки')
 
 
 def main(training: Training) -> None:
